9dee0736f0911723989f5ffa8297ddfc14a64ceb
Kiu82012 <john92359@gmail.com>
2024-10-06 21:26:43 +0800

    To do list
    Add ,delete  function

--- fyp/lib/main.dart
+++ fyp/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:clean_calendar/clean_calendar.dart';
 import 'package:flutter/material.dart';
+import 'package:fyp/ToDoList.dart';
+import 'package:fyp/dialog_box.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -30,7 +34,19 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0; // Index of the selected tab
   PageController _pageController = PageController();
 
+  //text controller
+  final _controller=TextEditingController();
   List<DateTime> selectedDates = [];
+  List toDoList = [
+    ["Make tutorial", false],
+    ["Do exercise", false],
+  ];
+ //checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -38,6 +54,34 @@ class _HomeState extends State<Home> {
       _pageController.jumpToPage(index);
     });
   }
+//save new task
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text,false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+//create a new task
+  void createNewTask(){
+    showDialog(context: context, builder: (context){
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: ()=> Navigator.of(context).pop(),);
+    });
+  }
+
+  //delete a task
+
+  void deleteTask(int index){
+    setState(() {
+      toDoList.removeAt(index);
+    });
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -46,18 +90,24 @@ class _HomeState extends State<Home> {
         elevation: 2,
         title: const Text("FYP Calendar ver1.1"),
       ),
+
       body: PageView(
         controller: _pageController,
-        children:  [
+        children: [
           Center(
             child: Text('Home Page'),
-
           ),
           CleanCalendar(
-            weekdaysSymbol: const Weekdays(sunday: "Sun", monday: "Mon", tuesday: "Tue", wednesday: "Wed", thursday: "Thur", friday: "Fri", saturday: "Sat"),
+            weekdaysSymbol: const Weekdays(
+                sunday: "Sun",
+                monday: "Mon",
+                tuesday: "Tue",
+                wednesday: "Wed",
+                thursday: "Thur",
+                friday: "Fri",
+                saturday: "Sat"),
             generalDatesProperties: DatesProperties(
-              datesDecoration: DatesDecoration(
-              ),
+              datesDecoration: DatesDecoration(),
               // Configuration for the CleanCalendar widget
             ),
             currentDateProperties: DatesProperties(
@@ -67,9 +117,30 @@ class _HomeState extends State<Home> {
               ),
             ),
           ),
-          Center(
-            child: Text('To-Do-List'),
+          Stack(
+            children: [
+              ListView.builder(
+              itemCount: toDoList.length,
+                itemBuilder: (BuildContext context, int index) {
+               return Todolist(
+                 taskName: toDoList[index][0],
+                 taskCompleted: toDoList[index][1],
+                  onChanged: (value) => checkBoxChanged(value, index),
+                deleteFunction: (context)=> deleteTask(index),
+               );
+            },
+              ),
+              Positioned(
+                bottom: 16.0,
+                right: 16.0,
+                child:FloatingActionButton(
+                onPressed: createNewTask,
+                child: Icon(Icons.add),
+              ),
+              )
+          ],
           ),
+
           Center(
             child: Text('Reminder'),
           ),
@@ -79,7 +150,9 @@ class _HomeState extends State<Home> {
             _selectedIndex = index;
           });
         },
+
       ),
+
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -90,7 +163,6 @@ class _HomeState extends State<Home> {
             icon: Icon(Icons.calendar_today),
             label: 'Calendar',
           ),
-
           BottomNavigationBarItem(
             icon: Icon(Icons.checklist),
             label: 'To-Do-List',
